c947e8fa71986236776a6edf483ff7b6e708f124
Ricardo Bermudez <ricardo.bermudez@mercado.libre.com>
2023-10-31 20:40:41 -0500

    delete example component

--- src/components/nabvar/navbar.spec.tsx
+++ src/components/nabvar/navbar.spec.tsx
@@ -1,13 +1,15 @@
 
- import { Navbar } from "./Navbar";
- import { createDOM } from "@builder.io/qwik/testing";
- import { test, expect } from "vitest";
+import { Navbar } from "./Navbar";
+import { createDOM } from "@builder.io/qwik/testing";
+import { test, expect } from "vitest";
 
 
-test("should render correctly", async () => {
- const { screen, render } = await createDOM();
-   await render(<Navbar />);
-   const planningLink = screen.querySelector('[role="planning"]')?.firstChild?.textContent;   
-    expect(planningLink).toContain('Planning');
-  });
+test("should render link planning", async () => {
+  const { screen, render } = await createDOM();
+  await render(<Navbar> </Navbar>);
+  const planningLink = screen.querySelector('[role="planning"]')?.firstChild as HTMLDivElement;
+  console.log("🚀 ~ file: navbar.spec.tsx:11 ~ test ~ planningLink:", planningLink?.textContent)
+
+  expect(planningLink?.textContent).toEqual('Planning');
+});
 
