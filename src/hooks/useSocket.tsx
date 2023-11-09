892c28d8fa3f8f2c8a5ed4de6f439a4a994440d9
Ricardo Bermudez <ricardo.bermudez@mercado.libre.com>
2023-11-09 13:32:44 -0500

    Add useSocket hook for real-time communication

--- src/hooks/useSocket.tsx
+++ src/hooks/useSocket.tsx
@@ -1,4 +1,4 @@
-import { useSignal } from "@builder.io/qwik";
+import { useComputed$, useSignal } from "@builder.io/qwik";
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000", { transports: ["websocket"] });
@@ -7,16 +7,21 @@ const useSocket = (nameEvent: string) => {
   const isConnect = useSignal<boolean>(false);
   const events = useSignal<Record<string, any>[]>([]);
 
-  socket.on("connect", () => {
-    isConnect.value = true;
-  });
+  useComputed$(() => {
+    socket.on("connect", () => {
+      isConnect.value = true;
+      console.log("connect");
+    });
 
-  socket.on("disconnect", () => {
-    isConnect.value = false;
-  });
+    socket.on("disconnect", () => {
+      isConnect.value = false;
+      console.log("disconnect");
+    });
 
-  socket.on(nameEvent, (data) => {
-    events.value = [...events.value, data];
+    socket.on(nameEvent, (data) => {
+      console.log("data", data);
+      events.value = [...events.value, data];
+    });
   });
 
   return { events, isConnect };
