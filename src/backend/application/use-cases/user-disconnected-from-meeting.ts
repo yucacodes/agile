b4d740f09cb42682a2107c3c5fe86d15d7d1efad
Ricardo Bermudez <ricardo.bermudez@mercado.libre.com>
2023-12-13 18:08:57 -0500

    Add UserDisconnectedFromMeetingDto and UserDisconnectedFromMeeting use case
    This commit adds a new DTO interface  and a new use case . The use case handles the disconnection of a user from a meeting by updating the meeting's participant status.

---
+++ src/backend/application/use-cases/user-disconnected-from-meeting.ts
@@ -0,0 +1,33 @@
+import { singleton } from 'tsyringe'
+import { UseCase } from './use-case'
+import type { UserDisconnectedFromMeetingDto } from '../dtos/user-disconnected-from-meeting-dto'
+import type { AuthInformationDto } from '../dtos'
+import { MeetingsRepository } from '@domain'
+
+@singleton()
+export class UserDisconnectedFromMeeting extends UseCase<
+  UserDisconnectedFromMeetingDto,
+  void
+> {
+  constructor(private meetingsRepository: MeetingsRepository) {
+    super()
+  }
+  async perform(
+    request: UserDisconnectedFromMeetingDto,
+    authInformation?: AuthInformationDto | undefined
+  ): Promise<void> {
+    const meeting = await this.meetingsRepository.fetchById(request.meetingId)
+
+    if (!meeting) {
+      throw new Error('Invalid meeting')
+    }
+
+    const participant = meeting.participantById(authInformation!.userId)
+
+    if (!participant) {
+      throw new Error('Invalid participant')
+    }
+
+    meeting.notifyParticipantDisconnected(authInformation!.userId)
+  }
+}
