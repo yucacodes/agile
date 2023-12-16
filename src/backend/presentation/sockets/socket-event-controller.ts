ee2d55cb0829f2b6f200574267b5b0ddec3b7f4a
Jorge Narvaez <jormelcn@gmail.com>
2023-12-16 01:48:52 -0500

    Merge socket-event-controller

--- src/backend/presentation/sockets/socket-event-controller.ts
+++ src/backend/presentation/sockets/socket-event-controller.ts
@@ -1,8 +1,7 @@
 import { plainToInstance } from 'class-transformer'
 import { validate } from 'class-validator'
 
-import { type UseCase } from '@application'
-
+import { type AuthInformationDto } from '@application'
 import { Logger } from '../logger'
 import { type GenericSocket, type SocketCallback } from './sockets-types'
 
@@ -10,65 +9,78 @@ interface RequestValidator<Request> {
   new (): Request & object
 }
 
-export interface HandleWithUseCaseArgs<Request, Result> {
-  socket: GenericSocket
-  request: Request
-  callback: SocketCallback<Result>
-  useCase: UseCase<Request, Result>
-  requestValidator: RequestValidator<Request>
+export interface SocketEventControllerConfig {
+  socketEvent: string
+  requestValidator: RequestValidator<any>
+  logger: Logger
 }
 
-export abstract class SocketEventController<
-  Event extends string,
-  Request,
-  Result,
-> {
-  private logger: Logger
+export type SocketEventControllerClass = {
+  new (...args: any): SocketEventController<any, any>
+}
 
-  constructor(private event: Event) {
-    this.logger = new Logger(`${event}EventController`)
+export abstract class SocketEventController<Request, Result> {
+  private config(): SocketEventControllerConfig {
+    throw new Error(
+      `Should configure ${this.constructor.name} using @socketEventController(config) decorator`
+    )
+  }
+
+  socketEvent() {
+    const { socketEvent } = this.config()
+    return socketEvent
   }
 
   public listenFor(socket: GenericSocket) {
-    const listener = (request: Request, callback: SocketCallback<Result>) => {
-      this.handle(socket, request, (result) => {
-        if (result.success) this.onSuccess(socket, result.data)
-        callback(result)
-      })
+    const { socketEvent, requestValidator, logger } = this.config()
+    const listener = async (
+      request: Request,
+      callback: SocketCallback<Result>
+    ) => {
+      const requestErrors = await validate(
+        plainToInstance(requestValidator, request)
+      )
+      if (requestErrors.length > 0) {
+        logger.error('Bad Request', requestErrors)
+        return callback({ success: false })
+      }
+
+      try {
+        const data = await this.handle(request, socket.data.auth)
+        logger.info('Success request')
+        this.onSuccess(socket, data)
+        return callback({ success: true, data })
+      } catch (error) {
+        logger.error('Request Error', error as Error)
+        return callback({ success: false })
+      }
     }
-    return [this.event, listener] as const
+    return [socketEvent as any, listener] as const
   }
 
   protected abstract handle(
-    socket: GenericSocket,
     request: Request,
-    callback: SocketCallback<Result>
-  ): Promise<void>
-
-  protected async handleWithUseCase({
-    socket,
-    request,
-    callback,
-    requestValidator,
-    useCase,
-  }: HandleWithUseCaseArgs<Request, Result>) {
-    const requestErrors = await validate(
-      plainToInstance(requestValidator, request)
-    )
-    if (requestErrors.length > 0) {
-      this.logger.error('Bad Request', requestErrors)
-      return callback({ success: false })
-    }
-    try {
-      const data = await useCase.perform(request, socket.data.auth)
-      this.logger.info('Use Case Success')
-      return callback({ success: true, data })
-    } catch (error) {
-      this.logger.error('Use Case Error', error as Error)
-      return callback({ success: false })
-    }
-  }
+    authData?: AuthInformationDto
+  ): Promise<Result>
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onSuccess(socket: GenericSocket, result: Result): void {}
+}
+
+export interface socketEventControllerProps {
+  socketEvent: string
+  requestValidator: RequestValidator<any>
+}
+
+export function socketEventController(props: socketEventControllerProps) {
+  return (constructor: SocketEventControllerClass) => {
+    const _config: SocketEventControllerConfig = {
+      socketEvent: props.socketEvent,
+      requestValidator: props.requestValidator,
+      logger: new Logger(`${props.socketEvent}EventController`),
+    }
+    constructor.prototype.config = function config() {
+      return _config
+    }
+  }
 }
