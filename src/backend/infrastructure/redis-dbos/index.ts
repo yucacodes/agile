6e9e08e60ad4fd7089f717e55ca34c9b4d68a459
Jorge Narvaez <jormelcn@gmail.com>
2024-03-02 12:57:53 -0500

    implement RedisRefreshTokenDbo and mapper

--- src/backend/infrastructure/redis-dbos/index.ts
+++ src/backend/infrastructure/redis-dbos/index.ts
@@ -1,3 +1,4 @@
 export * from './redis-meeting-dbo'
 export * from './redis-participant-dbo'
+export * from './redis-refresh-token-dbo'
 export * from './redis-voting-dbo'
--- src/backend/infrastructure/redis-dbos/redis-refresh-token-dbo.ts
+++ src/backend/infrastructure/redis-dbos/redis-refresh-token-dbo.ts
@@ -1,8 +1,45 @@
+import { RefreshToken } from '@domain'
+import { DboCollectionsMapper, dboMapper, type TimeProvider } from '@framework'
 import type { RedisEntityDbo } from './redis-entity-dbo'
+import { RedisEntityDboMapperHelper } from './redis-entity-dbo'
 
 export interface RedisRefreshTokenDbo extends RedisEntityDbo {
   secretHash: string
   expiresAt: number
   userId: string
   roles: string[]
+}
+
+@dboMapper({ model: RefreshToken })
+export class RedisRefreshTokenDboMapper extends DboCollectionsMapper {
+  constructor(
+    private timeProvider: TimeProvider,
+    private entityMapper: RedisEntityDboMapperHelper
+  ) {
+    super()
+  }
+
+  map(obj: RefreshToken): RedisRefreshTokenDbo {
+    const props = obj['props']
+    return {
+      ...this.entityMapper.map(obj),
+      expiresAt: props.expiresAt.getTime(),
+      secretHash: props.secretHash,
+      userId: props.userId,
+      roles: props.roles,
+    }
+  }
+
+  revert(dbo: RedisRefreshTokenDbo): RefreshToken {
+    return new RefreshToken(
+      {
+        ...this.entityMapper.revertProps(dbo),
+        expiresAt: new Date(dbo.expiresAt),
+        secretHash: dbo.secretHash,
+        userId: dbo.userId,
+        roles: dbo.roles,
+      },
+      this.timeProvider
+    )
+  }
 }
