d0528348a13cc22b733429e262415e31133efa0d
Ricardo Bermudez <ribermudez@petalmail.com>
2024-03-04 16:54:24 -0500

    add a type for paticipante table

--- src/components/players-table/PlayersTable.tsx
+++ src/components/players-table/PlayersTable.tsx
@@ -2,10 +2,17 @@ import { component$, useContext, useSignal, useTask$ } from '@builder.io/qwik'
 import { StateProvider } from '~/context/ProviderContext'
 import style from './players-table.module.css'
 
+import type { ParticipantDto } from '@application'
+
+interface Participant extends ParticipantDto {
+  points: number | null
+}
+
 export const PlayersTable = component$(() => {
   const state = useContext(StateProvider)
 
-  const participantsArr = useSignal<any[]>([])
+
+  const participantsArr = useSignal<Participant[]>([])
 
   useTask$(({ track }) => {
     track(() => state.participants)
@@ -14,7 +21,7 @@ export const PlayersTable = component$(() => {
     participantsArr.value = Object.values(state.participants).map(
       (participant) => {
         return {
-          name: participant.name,
+          ...participant,
           points: state.votes[participant.userId] || null,
         }
       }
@@ -37,11 +44,11 @@ export const PlayersTable = component$(() => {
                 manager
               </span>
             )}
-            {participant.points && (
+            {participant.points! > 0 && (
               <span class={`material-icons-outlined ${style.check}`}>done</span>
             )}
             <p>{participant.name}</p>
-            <p>{participant.points}</p>
+            <p>{ state.showVotes ? participant.points : '**'}</p>
           </div>
         ))}
       </div>
