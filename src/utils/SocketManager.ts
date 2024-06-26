24f774740320b089ae03da5827ae80d20c732b78
Jorge Narvaez <jormelcn@gmail.com>
2024-06-26 18:05:27 -0500

    Comment unused function

--- src/utils/SocketManager.ts
+++ src/utils/SocketManager.ts
@@ -5,34 +5,34 @@ import { ClientSocket, ListenEventsMap } from '@presentation'
 import { io } from 'socket.io-client'
 import { SocketResult } from '~/framework/presentation'
 
-export const emitEventWithCallback: QRL<
-  <T, P>(
-    socket: NoSerialize<ClientSocket>,
-    event: keyof ListenEventsMap,
-    data: T
-  ) => Promise<SocketResult<P>>
-> = $(
-  <T, P>(
-    socket: NoSerialize<ClientSocket>,
-    event: keyof ListenEventsMap,
-    data: T
-  ) => {
-    return new Promise<SocketResult<P>>((resolve, reject) => {
-      socket?.emit(event, data, (payload: SocketResult<P>) => {
-        if (!payload.success) {
-          reject(payload)
-        } else {
-          resolve(payload)
-        }
-      })
-    })
-  }
-)
+// export const emitEventWithCallback: QRL<
+//   <T, P>(
+//     socket: NoSerialize<ClientSocket>,
+//     event: keyof ListenEventsMap,
+//     data: T
+//   ) => Promise<SocketResult<P>>
+// > = $(
+//   <T, P>(
+//     socket: NoSerialize<ClientSocket>,
+//     event: keyof ListenEventsMap,
+//     data: T
+//   ) => {
+//     return new Promise<SocketResult<P>>((resolve, reject) => {
+//       socket?.emit(event, data, (payload: SocketResult<P>) => {
+//         if (!payload.success) {
+//           reject(payload)
+//         } else {
+//           resolve(payload)
+//         }
+//       })
+//     })
+//   }
+// )
 
 export const connectSocket: QRL<() => Promise<NoSerialize<ClientSocket>>> = $(
   async function (): Promise<NoSerialize<ClientSocket>> {
-    return new Promise((resolve, _) => {
-      let socket = noSerialize(
+    return new Promise((resolve) => {
+      const socket = noSerialize(
         io(import.meta.env.PUBLIC_API_URL, {
           transports: ['websocket'],
           protocols: ['websocket'],
